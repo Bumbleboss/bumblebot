8f3371a31147ec31b15a852ae440834cb7f05fdc
The Unnamed Engineer <theunnamedengineer@gmail.com>
2020-11-04 01:13:25 -0500

    General project cleanup.
    Signed-off-by: The Unnamed Engineer <theunnamedengineer@gmail.com>

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Basically the source code of my bot that's mainly used for my Discord server<br>
 Check out my server: https://discord.gg/7PCdKYN
 
 # IMPORTANT
-The bot source code is not meant to be used by others<br>
+The bot source code is not intended for use by others<br>
 No support will be provided whatsoever!
 
--- src/main/java/xyz/bumbleboss/bumblebot/Constants.java
+++ src/main/java/xyz/bumbleboss/bumblebot/Constants.java
@@ -7,19 +7,19 @@ import xyz.bumbleboss.core.Util;
 
 public class Constants {
 
-  public static String TOKEN = Config.getConfigVal("token").toString();
+  public static final String TOKEN = Config.getConfigVal("token").toString();
 
-  public static String COLOR = Config.getConfigVal("hex").toString();
+  public static final String COLOR = Config.getConfigVal("hex").toString();
 
-  public static String PREFIX = Config.getConfigVal("prefix").toString();
+  public static final String PREFIX = Config.getConfigVal("prefix").toString();
 
-  public static String SERVER_ID = getServer().get("id").toString();
+  public static final String SERVER_ID = getServer().get("id").toString();
 
-  public static String OWNER_ID = Config.getConfigVal("ownerId").toString();
+  public static final String OWNER_ID = Config.getConfigVal("ownerId").toString();
 
-  public static String HOST_ID = Config.getConfigVal("hostId").toString();
+  public static final String HOST_ID = Config.getConfigVal("hostId").toString();
 
-  public static String[] DEVS_ID = Util.JSONArrayTo((JSONArray) Config.getConfigVal("developersId"), String.class, Object::toString);
+  public static final String[] DEVS_ID = Util.JSONArrayTo((JSONArray) Config.getConfigVal("developersId"), String.class, Object::toString);
 
   public static JSONObject getServer() {
     return (JSONObject) Config.getConfigVal("server");
